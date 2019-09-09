5d17c49793f14f2f5be000c4b073aa97156a02d5
sergey@kletsko.com <sergey@kletsko.com>
2019-09-08 21:56:06 -0500

    First commit

--- string.py
+++ string.py
@@ -1,4 +1,28 @@
-string=("Apple")
+string = ("Apple")
 print(string)
 
 print("{} is best fruit ever!".format(string))
+
+favoriteFruits = ["Apple", "Mango", "Strawberry"]
+print(favoriteFruits)
+print(favoriteFruits[0])
+print(favoriteFruits[2])
+
+favoriteFruits[1] = "Orange"
+print(favoriteFruits[1])
+
+favoriteFruits.append("Kiwi")
+
+print(favoriteFruits)
+
+favoriteFruits.insert(1, "Mango")
+print(favoriteFruits[1])
+
+favoriteFruits.remove("Strawberry")
+print(favoriteFruits)
+
+favoriteFruits.sort()
+print(favoriteFruits)
+
+favoriteFruits.reverse()
+print(favoriteFruits)
